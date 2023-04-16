1431d000785c3df694b44f35a3e07a542ae4a5c0
Alex <thanhduonghd114@gmail.com>
2023-04-16 22:05:36 +0300

    done app bar and general functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,235 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Grade App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lightBlue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Grades'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String selectCourse = 'All Courses';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void closeDrawer() {
+    Navigator.pop(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
+        actions: [
+          Image.asset('images/profile_image.png'),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.menu),
+                tooltip: 'Close view',
+                onPressed: () {
+                  closeDrawer();
+                },
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'My Courses ',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ListTile(
+                  title:
+                      const Text('BM40A0202 Foundations of Computer Science'),
+                  onTap: () {
+                    // handle menu item 1 tap
+                  },
+                ),
+                ListTile(
+                  title: const Text('CT30A2803 User Interface and Usability'),
+                  onTap: () {
+                    // handle menu item 2 tap
+                  },
+                ),
+                ListTile(
+                  title: const Text('CT60A2411 Object-Oriented Programming'),
+                  onTap: () {
+                    // handle menu item 3 tap
+                  },
+                ),
+                ListTile(
+                  title: const Text('CT60A2500 Principles of C Programming'),
+                  onTap: () {
+                    // handle menu item 4 tap
+                  },
+                ),
+                ListTile(
+                  title: const Text('CT60A5521 Database Systems Management'),
+                  onTap: () {
+                    // handle menu item 5 tap
+                  },
+                ),
+                ListTile(
+                  title: const Text('CT60A9600 Discrete Models and Methods 2'),
+                  onTap: () {
+                    // handle menu item 6 tap
+                  },
+                ),
+              ],
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  '   Course',
+                  style: TextStyle(
+                    fontSize: 10.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 1.0),
+                DropdownButton<String>(
+                  isExpanded: true,
+                  items: const [
+                    DropdownMenuItem<String>(
+                      value: 'All Courses',
+                      child: Text('All Courses'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'User Interface and Usability',
+                      child: Text('User Interface and Usability'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'Foundation of Computer Science',
+                      child: Text('Foundation of Computer Science'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'Object-Oriented Programming',
+                      child: Text('Object-Oriented Programming'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'Principles of C Programming',
+                      child: Text('Principles of C Programming'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'Database System Management',
+                      child: Text('Database System Management'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'Discrtete Models and Methods',
+                      child: Text('Discrtete Models and Methods'),
+                    ),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      selectCourse = value.toString();
+                    });
+                  },
+                  hint: Text(selectCourse),
+                ),
+                const SizedBox(height: 7.0),
+                const Text(
+                  '   Choose the course to see the grades',
+                  style: TextStyle(
+                    fontSize: 10.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  "Current Progress",
+                  style: TextStyle(
+                    fontSize: 14,
+                    //fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 2),
+                LinearProgressIndicator(
+                  value: 0.75,
+                  backgroundColor: Colors.grey,
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    Color.fromARGB(255, 48, 91, 0),
+                  ),
+                  minHeight: 7,
+                ),
+                SizedBox(height: 8),
+                Text(
+                  "75% 4",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) {
+                return DataTable(
+                  columns: const [
+                    DataColumn(label: Text('Column 1')),
+                    DataColumn(label: Text('Column 2')),
+                  ],
+                  rows: [
+                    DataRow(cells: [
+                      DataCell(Text('Row ${index + 1}, Column 1')),
+                      DataCell(Text('Row ${index + 1}, Column 2')),
+                    ]),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
