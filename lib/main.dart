39aaaa2e0745c1301f7df441c5b0a03ecbc68a17
Alex <thanhduonghd114@gmail.com>
2023-04-17 18:52:17 +0300

    Done Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
@@ -47,61 +46,209 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-              ),
-              child: IconButton(
-                icon: const Icon(Icons.menu),
-                tooltip: 'Close view',
-                onPressed: () {
-                  closeDrawer();
-                },
+            SizedBox(
+              height: 370,
+              child: DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.menu),
+                      tooltip: 'Close view',
+                      onPressed: () {
+                        closeDrawer();
+                      },
+                    ),
+                    const SizedBox(height: 10.0),
+                    const Text(
+                      'LUT University - Moodle',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    ListTile(
+                      title: Row(
+                        children: const [
+                          Icon(Icons.dashboard_outlined),
+                          //Spacer(flex: 1),
+                          SizedBox(width: 10.0),
+                          Flexible(
+                            child: Text(
+                              'Dashboard',
+                            ),
+                          ),
+                        ],
+                      ),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      title: Row(
+                        children: const [
+                          Icon(Icons.home_outlined),
+                          //Spacer(flex: 1),
+                          SizedBox(width: 10.0),
+                          Flexible(
+                            child: Text(
+                              'Home',
+                            ),
+                          ),
+                        ],
+                      ),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      title: Row(
+                        children: const [
+                          Icon(Icons.calendar_today_outlined),
+                          //Spacer(flex: 1),
+                          SizedBox(width: 10.0),
+                          Flexible(
+                            child: Text(
+                              'Calendar',
+                            ),
+                          ),
+                        ],
+                      ),
+                      onTap: () {},
+                    ),
+                    ListTile(
+                      title: Container(
+                        color: const Color.fromARGB(255, 216, 244, 185),
+                        child: Row(
+                          children: const [
+                            Icon(Icons.grading_rounded),
+                            //Spacer(flex: 1),
+                            SizedBox(width: 10.0),
+                            Flexible(
+                              child: Text(
+                                'Grades',
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      onTap: () {},
+                    ),
+                  ],
+                ),
               ),
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
-                  'My Courses ',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
+                const Padding(
+                  padding: EdgeInsets.only(
+                      right: 1.5, left: 20, bottom: 10, top: 10),
+                  child: Text(
+                    'My Courses',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
                 ListTile(
-                  title:
-                      const Text('BM40A0202 Foundations of Computer Science'),
+                  title: Row(
+                    children: const [
+                      Icon(Icons.school),
+                      //Spacer(flex: 1),
+                      SizedBox(width: 10.0),
+                      Flexible(
+                        child: Text(
+                          'BM40A0202 Foundations of Computer Science',
+                        ),
+                      ),
+                    ],
+                  ),
                   onTap: () {
                     // handle menu item 1 tap
                   },
                 ),
                 ListTile(
-                  title: const Text('CT30A2803 User Interface and Usability'),
+                  //title: const Text('CT30A2803 User Interface and Usability'),
+                  title: Row(
+                    children: const [
+                      Icon(Icons.school),
+                      SizedBox(width: 10.0),
+                      Flexible(
+                        child: Text(
+                          'CT30A2803 User Interface and Usability',
+                        ),
+                      ),
+                    ],
+                  ),
                   onTap: () {
                     // handle menu item 2 tap
                   },
                 ),
                 ListTile(
-                  title: const Text('CT60A2411 Object-Oriented Programming'),
+                  //title: const Text('CT60A2411 Object-Oriented Programming'),
+                  title: Row(
+                    children: const [
+                      Icon(Icons.school),
+                      SizedBox(width: 10.0),
+                      Flexible(
+                        child: Text(
+                          'CT60A2411 Object-Oriented Programming',
+                        ),
+                      ),
+                    ],
+                  ),
                   onTap: () {
                     // handle menu item 3 tap
                   },
                 ),
                 ListTile(
-                  title: const Text('CT60A2500 Principles of C Programming'),
+                  //title: const Text('CT60A2500 Principles of C Programming'),
+                  title: Row(
+                    children: const [
+                      Icon(Icons.school),
+                      SizedBox(width: 10.0),
+                      Flexible(
+                        child: Text(
+                          'CT60A2500 Principles of C Programming',
+                        ),
+                      ),
+                    ],
+                  ),
                   onTap: () {
                     // handle menu item 4 tap
                   },
                 ),
                 ListTile(
-                  title: const Text('CT60A5521 Database Systems Management'),
+                  //title: const Text('CT60A5521 Database Systems Management'),
+                  title: Row(
+                    children: const [
+                      Icon(Icons.school),
+                      SizedBox(width: 10.0),
+                      Flexible(
+                        child: Text(
+                          'CT60A5521 Database Systems Management',
+                        ),
+                      ),
+                    ],
+                  ),
                   onTap: () {
                     // handle menu item 5 tap
                   },
                 ),
                 ListTile(
-                  title: const Text('CT60A9600 Discrete Models and Methods 2'),
+                  //title: const Text('CT60A9600 Discrete Models and Methods 2'),
+                  title: Row(
+                    children: const [
+                      Icon(Icons.school),
+                      SizedBox(width: 10.0),
+                      Flexible(
+                        child: Text(
+                          'CT60A9600 Discrete Models and Methods 2',
+                        ),
+                      ),
+                    ],
+                  ),
                   onTap: () {
                     // handle menu item 6 tap
                   },
@@ -119,11 +266,15 @@ class _MyHomePageState extends State<MyHomePage> {
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                const Text(
-                  '   Course',
-                  style: TextStyle(
-                    fontSize: 10.0,
-                    fontWeight: FontWeight.bold,
+                const Padding(
+                  padding:
+                      EdgeInsets.only(right: 8, left: 8, bottom: 1, top: 2),
+                  child: Text(
+                    'Course',
+                    style: TextStyle(
+                      fontSize: 10.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
                 const SizedBox(height: 1.0),
