5b5c9672b5012d3765a760ed27bd482143b49551
Alex <thanhduonghd114@gmail.com>
2023-04-17 19:46:11 +0300

    fix app bar color

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Text(widget.title),
         centerTitle: true,
         actions: [
