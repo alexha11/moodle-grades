a61906be9961e1374ff56c12f7cae93c4f7e9fba
Alex <thanhduonghd114@gmail.com>
2023-04-19 15:21:47 +0300

    add 4 tables

--- lib/body.dart
+++ lib/body.dart
@@ -1,117 +1,371 @@
 import 'package:flutter/material.dart';
 
-buildingTableCell(String text, int fontSize) {
+buildingTableCell(
+    String text, double fontSize, FontWeight fontWeight, BuildContext context) {
   return TableCell(
     verticalAlignment: TableCellVerticalAlignment.middle,
     child: Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Text(
-        text,
-        style: const TextStyle(
-          fontSize: 9,
-        ),
-      ),
+      child: text != 'feedbackIcon'
+          ? Center(
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontSize: fontSize,
+                  fontWeight: fontWeight,
+                  color: text == 'Missed' ? Colors.red : Colors.black,
+                ),
+              ),
+            )
+          : SizedBox(
+              height: 25,
+              width: 20,
+              child: Center(
+                child: IconButton(
+                  icon: const Icon(Icons.open_in_browser_outlined),
+
+                  //tooltip: 'click to see feedback',
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: const Text('Feedback'),
+                          content: const Text(
+                              'Hey,\n\nGood answers, but for question 3 your answer is too short. Next time try to meet the question requirements. (-0.5)\n\n- Alex'),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                              child: const Text('Okay'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ),
     ),
   );
 }
 
-buildingTable() {
-  return ListView.builder(
-    itemCount: 3,
-    itemBuilder: (BuildContext context, int index) {
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-              width: 1,
-            ),
-            borderRadius: const BorderRadius.all(Radius.circular(10)),
+buildingTableAssignments(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Table(
+      border: TableBorder.all(
+        width: 1,
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
+      columnWidths: const {
+        0: FlexColumnWidth(1.3),
+        1: FlexColumnWidth(1),
+        2: FlexColumnWidth(1),
+        3: FlexColumnWidth(1.1),
+        4: FlexColumnWidth(1),
+      },
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      children: [
+        TableRow(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+            color: Color(0x63C4CAA9),
           ),
-          child: Table(
-            border: TableBorder.symmetric(
-              inside: const BorderSide(width: 1),
-            ),
-            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-            children: [
-              const TableRow(
-                decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 136, 244, 13),
-                ),
-                children: [
-                  TableCell(
-                    verticalAlignment: TableCellVerticalAlignment.middle,
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Assignments',
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  TableCell(
-                    verticalAlignment: TableCellVerticalAlignment.middle,
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Grade',
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  TableCell(
-                    verticalAlignment: TableCellVerticalAlignment.middle,
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Range',
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  TableCell(
-                    verticalAlignment: TableCellVerticalAlignment.middle,
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'Deadline',
-                        style: TextStyle(
-                          fontSize: 10,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              ...List.generate(
-                5,
-                (index) => TableRow(
-                  children: [
-                    buildingTableCell('Assignment $index', 9),
-                    buildingTableCell('4.5', 9),
-                    buildingTableCell('0 - 5', 9),
-                    buildingTableCell('Submitted', 9),
-                  ],
-                ),
-              ),
-            ],
-          ),
+          children: [
+            buildingTableCell('Assignments', 10, FontWeight.bold, context),
+            buildingTableCell('Grade', 10, FontWeight.bold, context),
+            buildingTableCell('Range', 10, FontWeight.bold, context),
+            buildingTableCell('Feedback', 10, FontWeight.bold, context),
+            buildingTableCell('Deadline', 10, FontWeight.bold, context),
+          ],
         ),
-      );
-    },
+        TableRow(
+          children: [
+            buildingTableCell('Assignment 1', 9, FontWeight.normal, context),
+            buildingTableCell('4.5', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 5', 9, FontWeight.normal, context),
+            buildingTableCell('feedbackIcon', 9, FontWeight.normal, context),
+            buildingTableCell('Submitted', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Assignment 2', 9, FontWeight.normal, context),
+            buildingTableCell('0', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 5', 9, FontWeight.normal, context),
+            buildingTableCell('-', 9, FontWeight.normal, context),
+            buildingTableCell('Missed', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Assignment 3', 9, FontWeight.normal, context),
+            buildingTableCell('5', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 5', 9, FontWeight.normal, context),
+            buildingTableCell('Excellent', 9, FontWeight.normal, context),
+            buildingTableCell('Submitted', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Assignment 4', 9, FontWeight.normal, context),
+            buildingTableCell('7.25', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 8', 9, FontWeight.normal, context),
+            buildingTableCell('Great work', 9, FontWeight.normal, context),
+            buildingTableCell('Submitted', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell(
+                'Assignment 5 (Extra Credits)', 9, FontWeight.normal, context),
+            buildingTableCell('', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 5', 9, FontWeight.normal, context),
+            buildingTableCell('', 9, FontWeight.normal, context),
+            buildingTableCell(
+                '28 April 11:59 PM', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
+            color: Color(0x63C4CAA9),
+          ),
+          children: [
+            buildingTableCell('Total', 9, FontWeight.normal, context),
+            buildingTableCell('16.75', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 28', 9, FontWeight.normal, context),
+            buildingTableCell('', 9, FontWeight.normal, context),
+            buildingTableCell('', 9, FontWeight.normal, context),
+          ],
+        ),
+      ],
+    ),
   );
 }
 
+buildingTableQuizes(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Table(
+      border: TableBorder.all(
+        width: 1,
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
+      columnWidths: const {
+        0: FlexColumnWidth(1.3),
+        1: FlexColumnWidth(1),
+        2: FlexColumnWidth(1),
+        3: FlexColumnWidth(1),
+      },
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      children: [
+        TableRow(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+            color: Color(0x63C4CAA9),
+          ),
+          children: [
+            buildingTableCell('Quizes', 10, FontWeight.bold, context),
+            buildingTableCell('Grade', 10, FontWeight.bold, context),
+            buildingTableCell('Range', 10, FontWeight.bold, context),
+            buildingTableCell('Deadline', 10, FontWeight.bold, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Quiz 1', 9, FontWeight.normal, context),
+            buildingTableCell('1', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 5', 9, FontWeight.normal, context),
+            buildingTableCell('Submitted', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Quiz 2', 9, FontWeight.normal, context),
+            buildingTableCell('3.75', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 5', 9, FontWeight.normal, context),
+            buildingTableCell('Submitted', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Quiz 3', 9, FontWeight.normal, context),
+            buildingTableCell('0', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 5', 9, FontWeight.normal, context),
+            buildingTableCell('Missed', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Quiz 4', 9, FontWeight.normal, context),
+            buildingTableCell('4.25', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 5', 9, FontWeight.normal, context),
+            buildingTableCell('Submitted', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Quiz 5', 9, FontWeight.normal, context),
+            buildingTableCell('', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 5', 9, FontWeight.normal, context),
+            buildingTableCell(
+                '15 April 11:59 PM', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
+            color: Color(0x34C4CAA9),
+          ),
+          children: [
+            buildingTableCell('Total', 9, FontWeight.normal, context),
+            buildingTableCell('15', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 25', 9, FontWeight.normal, context),
+            buildingTableCell('', 9, FontWeight.normal, context),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+buildingTableProjects(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Table(
+      border: TableBorder.all(
+        width: 1,
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
+      columnWidths: const {
+        0: FlexColumnWidth(1.3),
+        1: FlexColumnWidth(1),
+        2: FlexColumnWidth(1),
+        3: FlexColumnWidth(1),
+      },
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      children: [
+        TableRow(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+            color: Color(0x63C4CAA9),
+          ),
+          children: [
+            buildingTableCell('Projects', 10, FontWeight.bold, context),
+            buildingTableCell('Grade', 10, FontWeight.bold, context),
+            buildingTableCell('Range', 10, FontWeight.bold, context),
+            buildingTableCell('Deadline', 10, FontWeight.bold, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Individual Project', 9, FontWeight.normal, context),
+            buildingTableCell('7.5', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 10', 9, FontWeight.normal, context),
+            buildingTableCell('Submitted', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Group Project', 9, FontWeight.normal, context),
+            buildingTableCell('', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 25', 9, FontWeight.normal, context),
+            buildingTableCell('23 April 11:59 PM', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
+            color: Color(0x34C4CAA9),
+          ),
+          children: [
+            buildingTableCell('Total', 9, FontWeight.normal, context),
+            buildingTableCell('7.5', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 35', 9, FontWeight.normal, context),
+            buildingTableCell('', 9, FontWeight.normal, context),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+buildingTableExams(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.all(8.0),
+    child: Table(
+      border: TableBorder.all(
+        width: 1,
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+      ),
+      columnWidths: const {
+        0: FlexColumnWidth(1.3),
+        1: FlexColumnWidth(1),
+        2: FlexColumnWidth(1),
+        3: FlexColumnWidth(1),
+      },
+      defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+      children: [
+        TableRow(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+            color: Color(0x63C4CAA9),
+          ),
+          children: [
+            buildingTableCell('Exams', 10, FontWeight.bold, context),
+            buildingTableCell('Grade', 10, FontWeight.bold, context),
+            buildingTableCell('Range', 10, FontWeight.bold, context),
+            buildingTableCell('Deadline', 10, FontWeight.bold, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Mid-term Exam', 9, FontWeight.normal, context),
+            buildingTableCell('80%', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 100%', 9, FontWeight.normal, context),
+            buildingTableCell('Submitted', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          children: [
+            buildingTableCell('Final Exam', 9, FontWeight.normal, context),
+            buildingTableCell('', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 100%', 9, FontWeight.normal, context),
+            buildingTableCell('6 May 11:59 PM', 9, FontWeight.normal, context),
+          ],
+        ),
+        TableRow(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
+            color: Color(0x34C4CAA9),
+          ),
+          children: [
+            buildingTableCell('Total', 9, FontWeight.normal, context),
+            buildingTableCell('80%', 9, FontWeight.normal, context),
+            buildingTableCell('0 - 200%', 9, FontWeight.normal, context),
+            buildingTableCell('', 9, FontWeight.normal, context),
+          ],
+        ),
+      ],
+    ),
+  );
+}
 
 class BuildingBody extends StatefulWidget {
   const BuildingBody({super.key});
@@ -124,8 +378,8 @@ class _BuildingBodyState extends State<BuildingBody> {
   String selectCourse = 'All Courses';
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
+    return ListView(
+      children: <Widget>[
         Container(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -213,7 +467,7 @@ class _BuildingBodyState extends State<BuildingBody> {
                 value: 0.75,
                 backgroundColor: Colors.grey,
                 valueColor: AlwaysStoppedAnimation<Color>(
-                  Color.fromARGB(255, 48, 91, 0),
+                  Color.fromARGB(100, 83, 102, 0),
                 ),
                 minHeight: 7,
               ),
@@ -232,9 +486,16 @@ class _BuildingBodyState extends State<BuildingBody> {
             ],
           ),
         ),
-        Expanded(
-          child: buildingTable(),
-        ),
+        Container(
+          child: Column(
+            children: [
+              buildingTableAssignments(context),
+              buildingTableQuizes(context),
+              buildingTableProjects(context),
+              buildingTableExams(context),
+            ],
+          ),
+        )
       ],
     );
   }
